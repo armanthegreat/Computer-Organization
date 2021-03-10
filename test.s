5b85a64ae92bb82a2b6b9da58d5774c265a1e63e
arman <arman.vala@gmail.com>
2021-03-10 20:29:08 +0100

    working on counter

--- test.s
+++ test.s
@@ -9,15 +9,16 @@
      action: .asciz "r" #string literal for using fopen
      format: .asciz "%d"
 
-#print:
-    # pushq     %rbp
-     #movq      %rsp,%rbp
+print:
+     pushq     %rbp
+     movq      %rsp,%rbp
 
-    # movq      $format,%rdi
-    # movq      $0,%rax
-    # call      printf
-     #popq      %rbp
-     #ret
+     movq      $format,%rdi
+     movq      $0,%rax
+     call      printf
+     popq      %rbp
+     ret
+
 
 fileread: #rdi set to filename in main 
 
@@ -58,20 +59,50 @@ fileread: #rdi set to filename in main
      popq      %rbp
      ret
 
+
 operationTable:
-
-     .quad     0x3E   # move up (>) #case0
-     .quad     0x3C   # move down(<) #case1
-     .quad     0x2B   # plus(+) #case2
-     .quad     0x2D   # minus(-) #case3
-     .quad     0x2D # Input (,) #case4
-     .quad     0x2E # Output (.) #case5
-     .quad     0x5B # start loop ([) #case6
-     .quad     0x5D # end loop (]) #case7
+     pushq     %rbp
+     movq      %rsp,%rbp
 
 
+     #.qu     0x3E   # move up (>) #case0
+     #.byte     0x3C   # move down(<) #case1
+     #.quad     0x2B   # plus(+) #case2
+     #.quad     0x2D   # minus(-) #case3
+     #.quad     0x2D # Input (,) #case4
+     #.quad     0x2E # Output (.) #case5
+     #.quad     0x5B # start loop ([) #case6
+     #.quad     0x5D # end loop (]) #case7
 
 
+     #plus
+     cmpb      $0x2B,%al
+     je        plus
+     popq      %rbp
+     ret  
+
+     plus:
+     movq      $1,%rsi
+     call      print
+     popq      %rbp
+     ret
+
+     
+counter: 
+     pushq     %rbp
+     movq      %rsp,%rbp
+
+     loop:
+          cmpq      $0,%rdi
+          je        loopend
+          decq      %rdi
+          movb      (%rsi),%cl #1 byte of the thingz
+          call      operationTable
+          incq      %rsi
+          jmp       loop
+     
+loopend:
+     ret
 
 .global main
 main:
@@ -83,8 +114,13 @@ main:
      
      call      fileread
 
-     movq      %rax,%r9
+     movq      %rax,%rdi #lenght 
 
+     movq      $buff,%rsi #address to the thingz 
+
+     
+
+     call     counter
 
      movq      $0,%rdi
      call      exit

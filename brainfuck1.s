6960b6c403b90f66a287e0119ea5f6459fd6a0b9
arman <arman.vala@gmail.com>
2021-03-16 19:38:37 +0100

    NO NESTED LOOP

--- brainfuck1.s
+++ brainfuck1.s
@@ -1,132 +1,135 @@
-.global brainfuck
-
-format_str: .asciz "We should be executing the following code:\n%s"
-
-# Your brainfuck subroutine will receive one argument:
-# a zero terminated string which is the code to execute.
 .text
 	readformat: .asciz "%lc"
 	printformat: .asciz "%c"
+
+
+.global brainfuck
 
 brainfuck:
 	pushq 	%rbp
 	movq 	%rsp, %rbp
 
-	movq	%rdi, %rcx # adress of string buffer 
+	movq	%rdi, %r14 # adress of string buffer 
 
-	movq 	$1000, %rdi  #creating 1000 cells initialized to 0 
+	movq 	$10000, %rdi  #creating 1000 cells initialized to 0 
  	movq 	$1 , %rsi    #each cell having the size of 1 byte
  	call 	calloc  
 
-	movq	%rax,%rdx #move pointer to first cell 
+	movq	%rax,%r15 #move pointer to first cell 
 
-	movq	$1,%r10 #Counter to compare number of opening and closing brackets 
+	movq	$0,%r13 #Counter to compare number of opening and closing brackets 
 
 	operations:
-			movb	(%rcx),%r9b
-			
-			cmpb	$0,%r9b
+			movb	(%r14),%r12b
+
+			cmpb	$0,%r12b
 			je		end
 
-			cmpb    $0x2B,%r9b #[+]
+			cmpb    $0x2B,%r12b #[+]
 			je		add
 
-			cmpb    $0x2D,%r9b #[-]
+			cmpb    $0x2D,%r12b #[-]
 			je		subtract
 
-			cmpb    $0x3E,%r9b#[>]
+			cmpb    $0x3E,%r12b#[>]
 			je		next
 			
-			cmpb    $0x3C,%r9b#[<]
+			cmpb    $0x3C,%r12b#[<]
 			je		previous
 
-			cmpb    $0x2E,%r9b#[.]
+			cmpb    $0x2E,%r12b#[.]
 			je		output
 
-			cmpb    $0x2C,%r9b
-			je		input
+			cmpb    $0x2C,%r12b
+			je		output #should be input
 
-			cmpb	$0x5B,%r9b
+			cmpb	$0x5B,%r12b
 			je		lpstart
 
-			cmpb	$0x5D,%r9b
+			cmpb	$0x5D,%r12b
 			je		lpend
 
-			incq	%rcx	#ignore invalid values (else)
+			incq	%r14	#ignore invalid values (else)
 			jmp		operations
 
 		add:
-			incq	(%rdx)
-			incq	%rcx 
+			incq	(%r15)
+			incq	%r14 
 			jmp		operations
 		
 		subtract:
-			decq 	(%rdx)
-			incq	%rcx
+			decq 	(%r15)
+			incq	%r14
 			jmp		operations
 		
 		next:
-			incq	%rdx
-			incq	%rcx
+			incq	%r15
+			incq	%r14
 			jmp		operations
 
 		previous:
-			decq	%rdx
-			incq	%rcx
+			decq	%r15
+			incq	%r14
 			jmp		operations
 
 
 		input:
 			movq    $readformat, %rdi    
-			movq    %rdx, %rsi            
+			movq    %r15, %rsi            
 			call    scanf  
-			incq	%rcx          	
+			incq	%r14          	
 			jmp     operations           
 
 		output:
 			movq    $printformat, %rdi    
-			movq    %rdx, %rsi          
+			movq    (%r15), %rsi          
 			movq    $0, %rax              
 			call    printf    
 
-			incq	%rcx         
+			incq	%r14     	    
 			jmp     operations
 
 		lpstart:
-			pushq	%rcx		#save adress of loop start
-			incq	%rcx
+			pushq	%r14		#save adress of loop start
+			incq	%r14
 			jmp		operations
 
 		lpend:
-			popq	%rcx
-			cmpq	$0,(%rdx)
+			popq	%r14
+			cmpq	$0,(%r15)
 			jne		lpstart
 			jmp		lpskip
+			
 
 		lpskip:
-			incq	%rcx
-			movb	(%rcx),%r9b
-
-			cmpb	$0x5B,%r9b #Compare current operation with [
+			movb	(%r14),%r12b
+			
+			cmpb	$0x5B,%r12b #Compare current operation with [
 			je		lpinc
 
-			cmpb	$0x5D,%r9b #Compare current operation with ]
+			cmpb	$0x5D,%r12b #Compare current operation with ]
 			je		lpdec
 
+			incq	%r14
 			jmp		lpskip
 
 		lpinc:		
-			incq	%r10
+			incq	%r13
+			incq	%r14
 			jmp		lpskip
 
 		lpdec:
-			decq	%r10
-			cmpq	$0,%r10
+			incq	%r14
+			decq	%r13
+			cmpq	$0,%r13
 			jne		lpskip
-			incq	%rcx
+
 			jmp		operations
 
+	
 		end:
 			movq	%rbp, %rsp
 			popq 	%rbp
 			ret
+
+			
